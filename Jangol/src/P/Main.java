d31fc6b6d2bb89de018508a2a999b7b62e83bef1
kyungbin22 <kb9617@gmail.com>
2024-12-30 16:36:11 +0900

    q9498

--- Jangol/src/P/Main.java
+++ Jangol/src/P/Main.java
@@ -1,9 +1,25 @@
 package P;
 
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
-        int N = -100;
-        System.out.println(N);
-
+    	Scanner sc = new Scanner(System.in);
+    	int score = sc.nextInt();
+    	if (score >= 90) {
+    		System.out.println("A");
+    	}
+    	else if (score >= 80) {
+    		System.out.println("B");
+    	}
+    	else if (score >= 70) {
+    		System.out.println("C");
+    	}
+    	else if (score >= 60) {
+    		System.out.println("D");
+    	}
+    	else 
+    	System.out.println("F");
+    	sc.close();
     }
 }
