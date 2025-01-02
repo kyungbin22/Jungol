d1737b11694879502e43bf87c7b6890690a2f3f0
kyungbin22 <kb9617@gmail.com>
2025-01-02 09:24:19 +0900

    q571

--- Jangol/src/jangol/Main.java
+++ Jangol/src/jangol/Main.java
@@ -4,16 +4,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[]args) {
-    	
-    	char[] characters = new char[10];	
-    	
     	Scanner sc = new Scanner(System.in);
-    	for (int i = 0; i < 10; i++) {
-    		characters[i] = sc.next().charAt(0);
+    	
+    	int num = sc.nextInt();
+    	
+    	for (int i = 0; i < num; i++) {
+    		//System.out.println("~!@#$^&*()_+|");
+    		Sub.printString();
+    		
     	}
-    	for (int i= 0; i < 10; i++) {
-    		System.out.print(characters[i]);
-    	}
-    	sc.close();
     }
 }
---
+++ Jangol/src/jangol/Sub.java
@@ -0,0 +1,7 @@
+package jangol;
+
+public class Sub {
+	public static void printString() {
+		System.out.println("~!@#$^&*()_+|");
+	}
+}
